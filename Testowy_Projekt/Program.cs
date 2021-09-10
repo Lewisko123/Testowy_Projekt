15d63233c595937a9769aa4c33fca023cc030332
Karol Deoniziak <karoldeoniziak1@gmail.com>
2021-09-10 11:34:03 +0200

    Dodaj pliki projektów.

---
+++ Testowy_Projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testowy_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
